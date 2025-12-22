d43c9fa15070cd9a9ebaf64dfd9ca1dff7d2a201
Mohamed Aref <maref.dev@gmail.com>
2025-12-22 13:18:06 +0000

    use this icon insted <svg xmlns="http://www.w3.org/2000/svg" width="24"

--- src/app/bottom-nav.tsx
+++ src/app/bottom-nav.tsx
@@ -3,7 +3,7 @@
 
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
-import { Home, CalendarDays, User, QrCode } from 'lucide-react';
+import { Home, CalendarDays, QrCode } from 'lucide-react';
 import { cn } from '@/lib/utils';
 
 const MapPinnedIcon = (props: React.SVGProps<SVGSVGElement>) => (
@@ -14,12 +14,19 @@ const MapPinnedIcon = (props: React.SVGProps<SVGSVGElement>) => (
     </svg>
 )
 
+const HeadsetIcon = (props: React.SVGProps<SVGSVGElement>) => (
+    <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" {...props}>
+        <path d="M3 11h3a2 2 0 0 1 2 2v3a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-5Zm0 0a9 9 0 1 1 18 0m0 0v5a2 2 0 0 1-2 2h-1a2 2 0 0 1-2-2v-3a2 2 0 0 1 2-2h3Z"/>
+        <path d="M21 16v2a4 4 0 0 1-4 4h-5"/>
+    </svg>
+)
+
 const navItems = [
   { href: '/', label: 'Home', icon: Home },
   { href: '/events', label: 'Events', icon: CalendarDays },
   { href: '/scan', label: 'Scan', icon: QrCode },
   { href: '/map', label: 'Map', icon: MapPinnedIcon },
-  { href: '/contact', label: 'Contact', icon: User },
+  { href: '/contact', label: 'Contact', icon: HeadsetIcon },
 ];
 
 export default function BottomNav() {
